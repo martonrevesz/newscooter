848de9c155196469173cea712a6fa7cb40b1492b
martonrevesz <martonrevesz@gmail.com>
2020-07-27 17:25:54 +0200

    add m+

--- scooter/src/home/Home.js
+++ scooter/src/home/Home.js
@@ -1,29 +1,48 @@
-import React from 'react';
-import logo from './logo.jpg'; 
-import './home.scss';
+import React from "react";
+import logo from "./logo.jpg";
+import "./home.scss";
 import { Col, Row } from "reactstrap";
-import Advantage from '../advantage/Advantage';
+import Advantage from "../advantage/Advantage";
 
 class Home extends React.Component {
-    render() {
-      return <div className="home">
-              <div className="auto-margin  why">
-                  <h1 className="text-left">Miért elektromos robogó?</h1>
-                  <p className="text-left">Elektromos robogók rövid és hosszútávú bérbeadásával foglalkozom Budapesten. 
-                    Hiszek abban, hogy az villanyrobogó bérlés a nagyvárosban kényelmes és praktikus alternatíva lehet az Ön számára akár alkalmilag, akár rendszeres munkavégzéshez. </p>
-                  <Row>
-                    <Col md="6">
-                      <img  class="home-image" src={logo} alt="Fehér Niu elektromos motor"/>
-                    </Col>
-                    <Col md="6" className="advantage">
-                      <Advantage title="Minimális üzemeltetési költség" detail="Míg egy átlagos kétütemű robogó 100 km-en 4 liter benzint és 1 deci olajat fogyaszt nagyjábol 1600ft üzemeltetési költséggel, addig egy villanyrobogó 3kWh áramot fogyaszt, ami mindossze 114 ft költséget jelent."/>
-                      <Advantage title="Megbízhatóság" detail="Motorjaink újak illetve az elektromos robogóknak minimális a szervízigénye, egyszerűsége folytán nem nagyon tud benne mi elromlani, így nincs többé lerobbant motor miatti időkiesés és bevételkiesés, cseremotorra való várakozás."/>
-                      <Advantage title="Kényelem" detail="Tempomat, telefontartó, modern LCD kijelző, tiszta, csendes üzemmód!"/>
-                    </Col>
-                  </Row>
-              </div>
-            </div>;
-    }
+  render() {
+    return (
+      <div className="home">
+        <div className="auto-margin  why">
+          <h1 className="text-left">Miért elektromos robogó?</h1>
+          <p className="text-left">
+            Elektromos robogók rövid és hosszútávú bérbeadásával foglalkozom
+            Budapesten. Hiszek abban, hogy a villanyrobogó bérlés a nagyvárosban
+            kényelmes és praktikus alternatíva lehet az Ön számára akár
+            alkalmilag, akár rendszeres munkavégzéshez.{" "}
+          </p>
+          <Row>
+            <Col md="6">
+              <img
+                class="home-image"
+                src={logo}
+                alt="Fehér Niu elektromos motor"
+              />
+            </Col>
+            <Col md="6" className="advantage">
+              <Advantage
+                title="Minimális üzemeltetési költség"
+                detail="Míg egy átlagos kétütemű robogó 100 km-en 4 liter benzint és 1 deci olajat fogyaszt nagyjábol 1600ft üzemeltetési költséggel, addig egy villanyrobogó 3kWh áramot fogyaszt, ami mindossze 114 ft költséget jelent."
+              />
+              <Advantage
+                title="Megbízhatóság"
+                detail="Motorjaink újak illetve az elektromos robogóknak minimális a szervízigénye, egyszerűsége folytán nem nagyon tud benne mi elromlani, így nincs többé lerobbant motor miatti időkiesés és bevételkiesés, cseremotorra való várakozás."
+              />
+              <Advantage
+                title="Kényelem"
+                detail="Tempomat, telefontartó, modern LCD kijelző, tiszta, csendes üzemmód!"
+              />
+            </Col>
+          </Row>
+        </div>
+      </div>
+    );
   }
+}
 
 export default Home;
--- scooter/src/scooters/Scooters.js
+++ scooter/src/scooters/Scooters.js
@@ -2,6 +2,7 @@ import React from "react";
 import scooter from "./scooter.jpg";
 import scooter2 from "./scooter2.jpg";
 import scooter3 from "./scooter3.jpg";
+import scooterm from "./scooterm.jpg";
 import "./scooters.scss";
 import { Col, Row } from "reactstrap";
 import Pricetable from "../pricetable/Pricetable.js";
@@ -48,7 +49,7 @@ class Scooters extends React.Component {
             </Col>
           </Row>
           <Pricetable day="3000" week="16000" />
-          <Row>
+          {/* <Row>
             <Col md="6" className="advantage">
               <p className="type">Niu N-Sport elektromos robogó:</p>
               <div className="advantage-row">
@@ -87,7 +88,47 @@ class Scooters extends React.Component {
               />
             </Col>
           </Row>
-          <Pricetable day="3500" week="18000" />
+          <Pricetable day="3500" week="18000" /> */}
+          <Row>
+            <Col md="6" className="advantage">
+              <p className="type">Niu M+ elektromos robogó:</p>
+              <div className="advantage-row">
+                <span className="icon">
+                  <i class="fas fa-tachometer-alt"></i>
+                </span>
+                <div className="info-text">
+                  <p>Maximum sebesség:</p>
+                  <p> 52 km/h</p>
+                </div>
+              </div>
+              <div className="advantage-row">
+                <span className="icon">
+                  <i class="fas fa-dot-circle"></i>
+                </span>
+                <div className="info-text">
+                  <p>Hatótáv:</p>
+                  <p> 125 km</p>
+                </div>
+              </div>
+              <div className="advantage-row">
+                <span className="icon dollar">
+                  <i class="fas fa-dollar-sign"></i>
+                </span>
+                <div className="info-text">
+                  <p>Üzemeltetési költség:</p>
+                  <p> ~ 1 ft / km</p>
+                </div>
+              </div>
+            </Col>
+            <Col md="6">
+              <img
+                src={scooterm}
+                width="300"
+                alt="Fehér Niu elektromos motor"
+              />
+            </Col>
+          </Row>
+          <Pricetable day="3000" week="16000" />
           <div className="text-left">
             Hosszabb távú vagy rendszeres bérlés esetén kérje el Önnek szóló
             egyedi ajánlatomat.
